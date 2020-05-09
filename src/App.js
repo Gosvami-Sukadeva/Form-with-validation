0cac1d7f3cba8aef362729f37e8914e577cf54c7
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-09 13:09:41 +0200

    validation of the form - addition to the structure error messages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    username: "",
+    email: "",
+    pass: "",
+    accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    },
+  };
+
+  messages = {
+    username_incorrect:
+      "Nazwa musi być dłuższa niż 10 znaków i nie zawierać spacji",
+    email_incorrect: "Brak @ e emailu",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    accept_incorrect: "Niepotwierdzona zgoda",
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+      // const checked = e.target.checked;
+      this.setState({
+        [name]: value,
+      });
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked,
+      });
+    }
+  };
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log("działa");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleOnSubmit} noValidate>
+          <label htmlFor="user">
+            Twoje imię:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.username && (
+              <span>{this.messages.username_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="email">
+            Twój email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.email && (
+              <span>{this.messages.email_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="password">
+            Twoje hasło:
+            <input
+              type="password"
+              id="password"
+              name="pass"
+              value={this.state.pass}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.pass && (
+              <span>{this.messages.password_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />
+            Wyrażam zgodę wszelaką
+          </label>
+          {this.state.errors.accept && (
+            <span>{this.messages.accept_incorrect}</span>
+          )}
+          <button>Zapisz się</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
